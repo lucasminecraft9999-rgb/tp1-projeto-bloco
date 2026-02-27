c5fb8b1e94f57a2e37de5e574b7c963cc7b607cd
lucasminecraft9999-rgb <lucasminecraft9999@gmail.com>
2026-02-27 16:48:52 -0300

    comecando-TP1

---
+++ src/main/java/com/example/tp1_projeto_bloco/Tp1ProjetoBlocoApplication.java
@@ -0,0 +1,13 @@
+package com.example.tp1_projeto_bloco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp1ProjetoBlocoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp1ProjetoBlocoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tp1_projeto_bloco/Tp1ProjetoBlocoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tp1_projeto_bloco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp1ProjetoBlocoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
